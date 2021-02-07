e2c8fcc84a2b0f4af5f13495d0f0b32791ac86c1
ezito developer <33319787+ezitoir@users.noreply.github.com>
2021-02-07 13:28:06 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,53 @@
 
 
+
+
+const express = require("express");
+const exhnbs  = require("express-handlebars");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressMiddleware = require("express-middleware");
+const expressSession = require("express-session");
+const expressSslify = require("express-sslify");
+const http = require("http");
+const https = require("https");
+const httpErrors = require("http-errors");
+const less = require("less");
+const multer = require("multer");
+const mysql = require("mysql");
+const nodeHtmlParser = require("node-html-parser");
+const socketio = require("socket.io");
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
+const redis = require("redis");
+const connectRedis = require("connect-redis");
+const ioredis = require("ioredis");
+
+
+require("./server/config/set-config")(
+    {        
+        express  , 
+        exhnbs  , 
+        bodyParser , 
+        cookieParser , 
+        expressMiddleware,
+        expressSession ,
+        expressSslify ,
+        http ,
+        https ,
+        httpErrors ,
+        less ,
+        multer ,
+        mysql ,
+        nodeHtmlParser , 
+        socketio ,
+        morgan,
+        redis ,
+        connectRedis,
+        ioredis ,
+        fs ,
+        path ,
+        __dirname
+    }
+);
